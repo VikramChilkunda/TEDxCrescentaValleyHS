56f44846db88102d3f6033719025324bd46604aa
EC2 Default User <ec2-user@ip-172-31-33-199.us-west-2.compute.internal>
2020-03-24 02:38:45 +0000

    f

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
+$(document).ready(function() {
+    $(document).scroll(function(){
+        if($(window).scrollTop() > 50){
+            $(".bg-light").attr("id", "scrollNav");
+        }
+        else{
+            $(".bg-light").attr("id", "");
+        }
+    });
+    if($(window).scrollTop() > 50){
+        $(".bg-light").attr("id", "scrollNav");
+    }
+    else{
+        $(".bg-light").attr("id", "");
+    }
+})
